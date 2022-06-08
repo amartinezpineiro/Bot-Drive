ec236768870e43ce10803c7414e13cc7bf07a45b
Adrian Martinez <amartinezpineiro@danielcastelao.org>
2022-06-08 12:07:17 +0200

    funciona

--- src/main/java/EjemploBot.java
+++ src/main/java/EjemploBot.java
@@ -1,23 +1,65 @@
+
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+import discord4j.rest.util.Color;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class EjemploBot {
-    public static void main(final String[] args) {
-        final String token = "OTUzNjI4NTg3Njg3Mjg0NzM2.YjHV1A.SEwLO_D5dmRAGR6sfNKbe6qx1Fo";//Se crea una variable String para almacenar el token
-        final DiscordClient client = DiscordClient.create(token);//Se crea el cliente utilizado el token
-        final GatewayDiscordClient gateway = client.login().block();//Se crea una pasarela usando el cliente
+
+    public static void main(String[] args) {
+        //Se crea una variable String para almacenar el token
+        final String token = args[0];
+
+        //Se crea el cliente utilizado el token
+        final DiscordClient client = DiscordClient.create(token);
+
+        //Se crea una pasarela usando el cliente
+        final GatewayDiscordClient gateway = client.login().block();
+
+        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                .color(Color.GREEN)
+                .title("Yoda")
+                .image("attachment://bot_bbyoda.jpg")
+                .build();
 
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();//Se crea una variable Message para recivir el mensage recivido
-            if ("!ping".equals(message.getContent())) {//Comprobacion de si el mensaje recivido equivale a !ping
-                final MessageChannel channel = message.getChannel().block();
-                channel.createMessage("Pong!").block();//Se crea el mensaje de respuesta Pong!
-            }
-        });
 
-        gateway.onDisconnect().block();//Se cierra la pasarela
+            //Se crea una variable Message para guardar el mensage recibido
+            final Message message = event.getMessage();
+
+            //Comprobación de si el mensaje recibido equivale a "!ping"
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                //Se crea el mensaje de respuesta Pong!
+                channel.createMessage("Pong!").block();
+            }
+            //Comprobación de si el mensaje recibido equivale a "!embed"
+            if ("!embed".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+
+                InputStream fileAsInputStream = null;
+                try {
+                    fileAsInputStream = new FileInputStream("/home/dam1/git/ExamenCod/src/main/resources/aux.jpeg");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                ;
+                channel.createMessage(MessageCreateSpec.builder()
+                        .content("content? content")
+                        .addFile("bot_bbyoda.jpg", fileAsInputStream)
+                        .addEmbed(embed)
+                        .build()).subscribe();
+            }
+
+        });
+        //Se cierra la pasarela
+        gateway.onDisconnect().block();
     }
 }
